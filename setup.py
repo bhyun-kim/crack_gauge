c8bf72221116d34796d8a6cf74e2672134acf9dd
uos-bhkim <shdik2002@uos.ac.kr>
2023-05-04 20:13:18 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Crack GAIGE',
+    version='0.1',
+    packages=find_packages(),
+    # install_requires=[
+    #     'numpy>=1.16.0',
+    #     'pandas>=0.24.0',
+    #     'matplotlib>=3.0.0'
+    # ],
+    # entry_points={
+    #     'console_scripts': [
+    #         'myproject=myproject.__main__:main'
+    #     ]
+    # },
+    # author='Your Name',
+    # author_email='youremail@example.com',
+    # description='A short description of your project',
+    # url='https://github.com/yourusername/myproject',
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    # ],
+    # python_requires='>=3.6',
+)
