b4198986ba682dfc437cba053963d6568e1c9898
bhyun-kim <94029750+bhyun-kim@users.noreply.github.com>
2023-08-11 14:54:48 -0500

    Update cgent_1024x1024.py

--- configs/deep_crack/cgent_1024x1024.py
+++ configs/deep_crack/cgent_1024x1024.py
@@ -9,12 +9,40 @@ pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/cgnet/cgnet_512
 crop_size = (1024, 1024)
 data_preprocessor = dict(size=crop_size)
 
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(1024, 768),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.99),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale_factor=2.0, keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+train_dataloader = dict(
+    dataset=dict(pipeline=train_pipeline)
+)
+val_dataloader = dict(
+    dataset=dict(pipeline=test_pipeline)
+)
+test_dataloader = val_dataloader
+
+
 model = dict(
     data_preprocessor=data_preprocessor,
     decode_head=dict(
         num_classes=2,
         loss_decode=dict(class_weight=[10., 20.])))
-        
+
 # optimizer
 optimizer = dict(type='Adam', lr=0.001, eps=1e-08, weight_decay=0.0005)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
